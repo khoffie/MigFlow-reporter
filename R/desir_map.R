feb31f41f586941f428083d8efe20f4f98cf2911
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-02-02 19:09:33 +0100

    desir_map() exported

--- R/desir_map.R
+++ R/desir_map.R
@@ -1,30 +1,38 @@
-## desir_map <- function(dt, type = c("desir", "net", "pred")) {
-##   agegroup <- desirability <- net <- pop_all <- net_pred <- NULL
-##   type <- match.arg(type)
-##   age <- dt[, unique(agegroup)]
-##   if(type == "desir") {
-##     map <- ggplot2::ggplot(helpeR::set_geom(dt, F)) +
-##       geom_sf(aes(fill = desirability)) +
-##       ggtitle(sprintf("%s, estimated desirability", age))
-##   }
-##   if(type == "net") {
-##     map <- ggplot2::ggplot(helpeR::set_geom(dt, F)) +
-##       geom_sf(aes(fill = net / pop_all * 100)) +
-##       ggtitle(sprintf("%s, Net migration", age))
-##   }
-##   if(type == "pred") {
-##     map <- ggplot2::ggplot(helpeR::set_geom(dt, F)) +
-##       geom_sf(aes(fill = net_pred / pop_all * 100)) +
-##       ggtitle(sprintf("%s, Predicted net migration", age))
-##   }
-##   map <- map +
-##     theme_bw() +
-##     theme(legend.position = "top")
-##   return(map)
-## }
+##' Makes desirability maps in report
+##' @param dt data.table
+##' @param type character, what should be shown? Estimated
+##'   desirability or net migration
+##' @return ggplots
+##' @author Konstantin Hoffie
+##' @export make_desirmaps
+##' @import ggplot2
+make_desirmaps <- function(dt, type = c("desir", "net")) {
+  des <- desir_map(dt, type[1])
+  net <- desir_map(dt, type[2])
+  return(des + net)
+}
 
-## make_desirmaps <- function(dt, type = c("desir", "net")) {
-##   des <- desir_map(dt, type[1])
-##   net <- desir_map(dt, type[2])
-##   return(des + net)
-## }
+desir_map <- function(dt, type = c("desir", "net", "pred")) {
+  agegroup <- desirability <- net <- pop_all <- net_pred <- NULL
+  type <- match.arg(type)
+  age <- dt[, unique(agegroup)]
+  if(type == "desir") {
+    map <- ggplot2::ggplot(helpeR::set_geom(dt, FALSE)) +
+      geom_sf(aes(fill = desirability)) +
+      ggtitle(sprintf("%s, estimated desirability", age))
+  }
+  if(type == "net") {
+    map <- ggplot2::ggplot(helpeR::set_geom(dt, FALSE)) +
+      geom_sf(aes(fill = net / pop_all * 100)) +
+      ggtitle(sprintf("%s, Net migration", age))
+  }
+  if(type == "pred") {
+    map <- ggplot2::ggplot(helpeR::set_geom(dt, FALSE)) +
+      geom_sf(aes(fill = net_pred / pop_all * 100)) +
+      ggtitle(sprintf("%s, Predicted net migration", age))
+  }
+  map <- map +
+    theme_bw() +
+    theme(legend.position = "top")
+  return(map)
+}
